47dbea90dede0f59d1b6ee0542dd7c91ffcfbece
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-09-15 11:16:50 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./features/counter/counterSlice";
+import {
+  decrement as decrement2,
+  increment as increment2,
+  incrementByAmount as incrementByAmount2,
+} from "./features/counter2/counterSlice";
+import { useAppDispatch, useAppSelector } from "./hooks";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const store = useAppSelector((state) => state);
+  const value = useAppSelector((state) => state.counter.value);
+  // const prevValue = useAppSelector((state) => state.counter.prevValue);
+  const value2 = useAppSelector((state) => state.testtest.value);
+  // const prevValue2 = useAppSelector((state) => state.testtest.prevValue);
+  const incrementAction = increment();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {JSON.stringify(store)}
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementAction)}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      <div>{value}</div>
+      {/* <div>{prevValue}</div> */}
+
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment2())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount2(5))}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement2())}
+        >
+          Decrement
+        </button>
+      </div>
+      <div>{value2}</div>
+      {/* <div>{prevValue2}</div> */}
     </div>
   );
 }
---
+++ src/features/counter2/counterSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface CounterState {
+  value: number;
+  prevValue: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+  prevValue: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter2",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.prevValue = state.value;
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.prevValue = state.value;
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.prevValue = state.value;
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
