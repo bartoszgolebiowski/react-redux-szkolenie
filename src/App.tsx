e2f6a0542fcf02ec5b1f0a281582e34507b01649
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-09-15 13:21:43 +0200

    redux async

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,19 @@
 import React from "react";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from "./features/counter/counterSlice";
-import {
-  decrement as decrement2,
-  increment as increment2,
-  incrementByAmount as incrementByAmount2,
-} from "./features/counter2/counterSlice";
+import { fetchProducts } from "./features/counter/counterSlice";
 import { useAppDispatch, useAppSelector } from "./hooks";
 
 function App() {
   const dispatch = useAppDispatch();
   const store = useAppSelector((state) => state);
-  const value = useAppSelector((state) => state.counter.value);
-  // const prevValue = useAppSelector((state) => state.counter.prevValue);
-  const value2 = useAppSelector((state) => state.testtest.value);
-  // const prevValue2 = useAppSelector((state) => state.testtest.prevValue);
-  const incrementAction = increment();
-  
+
+  React.useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
   return (
     <div>
-      {JSON.stringify(store)}
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(incrementAction)}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(incrementByAmount(5))}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-      <div>{value}</div>
-      {/* <div>{prevValue}</div> */}
-
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment2())}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(incrementByAmount2(5))}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement2())}
-        >
-          Decrement
-        </button>
-      </div>
-      <div>{value2}</div>
-      {/* <div>{prevValue2}</div> */}
+      <div>{JSON.stringify(store)}</div>
     </div>
   );
 }
-
 export default App;
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,14 +1,29 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { getTrendingProducts, TrendingProduct } from "../../product";
 
 export interface CounterState {
   value: number;
   prevValue: number;
+  isLoading: boolean;
+  isError: boolean;
+  data: TrendingProduct[];
 }
 
 const initialState: CounterState = {
   value: 0,
   prevValue: 0,
+  isLoading: false,
+  isError: false,
+  data: [],
 };
+
+// First, create the thunk :)
+export const fetchProducts = createAsyncThunk(
+  "counter/fetchProducts",
+  async () => {
+    return getTrendingProducts();
+  }
+);
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -27,8 +42,25 @@ export const counterSlice = createSlice({
       state.value += action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.data = [];
+        //console.log(action);
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.data = [];
+      });
+  },
 });
-
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
-
 export default counterSlice.reducer;
