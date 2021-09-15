cc950fe6dcf5eeb624f6f01ace17d0ab12d9e0bf
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-09-15 13:23:50 +0200

    redux async

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
-import counterReducer2 from "./features/counter2/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    testtest: counterReducer2,
   },
 });
 
